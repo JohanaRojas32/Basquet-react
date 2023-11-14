b61842c801f904b26fd93bd5c1159bd9ccbb757e
Johana Rojas <joohanarojas96@gmail.com>
2023-11-14 16:57:40 -0600

    funcionamiento carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -25,11 +25,11 @@ const Carousel = () => {
         </div>
       </div>
     </div>
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
       <span className="visually-hidden">Previous</span>
     </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
       <span className="carousel-control-next-icon" aria-hidden="true"></span>
       <span className="visually-hidden">Next</span>
     </button>
